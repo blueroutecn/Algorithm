f81d1dc5ac571d4bca5fa42a8211afcddbdcc603
JisungPark <teran0729@gamil.com>
2018-08-04 02:27:16 +0900

    commit

--- Memoization/LIS.cpp
+++ Memoization/LIS.cpp
@@ -14,6 +14,11 @@ int tmp;
 vector<int> seq;
 vector<int> optimal;
 
+/*
+문제에는 없지만,
+순열길이가 0 인경우, 순열이 존재하지 않는 경우도 고려해서 풀 수 있어야.
+0인경우는 findseq 함수를 돌리기전에 체크해서 순열이 없다고 출력해야 할 듯.
+*/
 int findseq(int idx){
 
   /*
@@ -21,18 +26,42 @@ int findseq(int idx){
   0. idx에 대한 max sequence length 찾아져 있으면 그 값 return 후 종료
   1. idx값 기준으로 seq[idx] 보다 큰 값 나오면 재귀 돌리기
   */
-  if(idx>=seq.size())
-    return 0;
-
-  if(optimal.at(idx) != -1)
+  //cout<<"function run: "<<idx<<" "<<endl;
+  vector<int>::iterator ret = optimal.begin() + idx;
+  if(idx==seq.size() - 1){
+    //cout<<"here1"<<endl;
+    *ret = 1;
+    return *ret;
+  }
+  if(optimal.at(idx) != -1){
+    //cout<<"here2"<<endl;
     return optimal.at(idx);
+  }
 
-  int& ret = optimal
+  bool flag = false;
   for(int i = idx + 1;i<seq.size();++i){
     if(seq[i]>seq[idx]){
-      ret = 1 + findseq(i)
+      //cout<<"here3"<<endl;
+      tmp = 1 + findseq(i);
+      if(*ret < tmp){
+        //cout<<"here4"<<endl;
+        *ret = tmp;
+        flag = true;
+      }
+    }
+    else{
+      //cout<<"here5"<<endl;
+      findseq(i);
     }
   }
+
+  if(!flag){
+    //cout<<"here6"<<endl;
+    *ret = 1;
+  }
+
+  return *ret;
+
 }
 
 
@@ -40,10 +69,10 @@ int main(){
 
 
   cin>>ncase;
-  cout<<ncase<<endl;
+  //cout<<ncase<<endl;
   for(int i = 0;i<ncase;++i){
     cin>>nelement;
-    cout<<nelement<<endl;
+    //cout<<nelement<<endl;
     for(int j = 0;j<nelement;++j){
       cin>>tmp;
       seq.push_back(tmp);
@@ -53,18 +82,14 @@ int main(){
       optimal.push_back(-1);
     }
 
-    for(int j = 0;j<seq.size();++j){
-      cout<<seq.at(j)<<" ";
-    }
-    cout<<endl;
+    findseq(0);
+
+    int opt = *max_element(optimal.begin(),optimal.end());
+    cout<<opt<<endl;
+
     seq.clear();
     optimal.clear();
-
-
-    //cout << typeid(vector<int>()).name()<<endl;
-
-    cout<<"seq: "<<seq.size()<<" "<<seq.capacity()<<endl;
-    //cout<<"empty: "<<vv.size()<<" "<<vv.capacity()<<endl;
+    tmp = -1;
   }
 
   vector<int>().swap(seq);
--- Memoization/tmp.cpp
+++ Memoization/tmp.cpp
@@ -8,12 +8,23 @@ using namespace std;
 
 int main(){
 
-  vector<int> v(5);
-  for(int i = 0;i<v.size();i++)
+  vector<int> v;
+
+  for(int i = 0;i<5;i++)
     v.push_back(i);
 
-  for(int i = 0;i<v.size();i++)
-    cout<<v.at(i)<<endl;
+  for(int i = 0;i<5;i++)
+    cout<<v.at(i)<<" ";
+  cout<<endl;
+
+  vector<int>::iterator iter = v.begin() + 3;
+  cout<<*iter<<endl;
+  *iter = 2;
+  cout<<*iter<<endl;
+
+  for(int i = 0;i<5;i++)
+    cout<<v.at(i)<<" ";
+  cout<<endl;
 
 
 }
