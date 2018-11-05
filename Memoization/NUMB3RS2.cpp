23efa6bb143f2f327aa2048853d7882d536f579e
jspark <teran0729@unist.ac.kr>
2018-11-05 22:14:38 +0900

    solve problem 181105

--- Memoization/NUMB3RS2.cpp
+++ Memoization/NUMB3RS2.cpp
@@ -1,41 +1,52 @@
 #include <iostream>
 #include <vector>
 #include <string.h>
+#include <algorithm>
 
 using namespace std;
 
 
 int map[50][50];
+double cache[50][51];
 double deg[50];
 
-double search(vector<int>& path,int n,int d,int q){
+double search(vector<int>& path,int n,int d,int t){
 
-  if(path.size() == d+1){
-    if(path.back() != q){
-      return 0;
-    }
 
-    double ret = 1;
-    for(int i = 0;i<path.size()-1;i++){
-      ret *=1/deg[path[i]];
-    }
+  double& ret = cache[path.back()][path.size()-1];
+  // cout<<"here1"<<endl;
+  if(ret!=-1){
+    // cout<<"here1 nan: "<<ret<<endl;
     return ret;
   }
 
+  // cout<<"here2"<<endl;
+  if(path.size() == d+1){
+    if(path.back() == t){
+      //cout<<"here1 nan: "<<nan<<endl;
+      ret = 1;
+      return ret;
+    }
+    else{
+      ret = 0;
+      return ret;
+    }
+  }
 
-  double prob = 0;
-
+  // cout<<"here3"<<endl;
+  ret = 0;
   int now = path.back();
   for(int i = 0;i<n;i++){
     if(map[now][i] == 1){
+      double p = 1/deg[now];
+      //cout<<p<<" "<<endl;
       path.push_back(i);
-      prob += search(path,n,d,q);
+      ret += p*search(path,n,d,t);
       path.pop_back();
     }
   }
 
-  return prob;
-
+  return ret;
 }
 
 
@@ -57,11 +68,30 @@ int main(){
     p = 0;
 
     for(int i = 0;i<50;i++)
-      memset(&map[i],0,sizeof(map[i]));
+      memset(&map[i],0,sizeof(int)*50);
+    fill(&cache[0][0],&cache[49][51],-1);
+
     memset(target,0,sizeof(target));
     memset(deg,0,sizeof(deg));
 
+
+
     cin>>n>>d>>p;
+
+    // for(int i = 0;i<n;i++){
+    //   for(int j = 0;j<n;j++){
+    //     cout<<cache[i][j]<<" ";
+    //   }
+    //   cout<<endl;
+    // }
+    //
+    // for(int i = 0;i<n;i++){
+    //   for(int j = 0;j<n;j++){
+    //     cout<<map[i][j]<<" ";
+    //   }
+    //   cout<<endl;
+    // }
+
     for(int i = 0;i<n;i++){
       for(int j = 0;j<n;j++){
         cin>>map[i][j];
@@ -71,10 +101,12 @@ int main(){
 
 
     cin>>t;
+    //cout<<"target: ";
     for(int i = 0;i<t;i++){
       cin>>target[i];
+      //cout<<target[i]<<" ";
     }
-
+    //cout<<endl;
 
 
     for(int i = 0;i<n;i++){
@@ -89,7 +121,12 @@ int main(){
     for(int i = 0;i<t;i++){
       vector<int> path;
       path.push_back(p);
-      cout<<search(path,n, d, target[i])<<" ";
+      cout.setf(ios::fixed);
+      cout.precision(8);
+      cout<<search(path,n, d, target[i]);
+      if(i!=t-1)
+        cout<<" ";
+      fill(&cache[0][0],&cache[49][51],-1);
     }
     cout<<endl;
 
