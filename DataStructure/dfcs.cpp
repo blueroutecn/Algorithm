e1ea8c2a5d23e4d5dd103f70a176e4870e2a5fa1
jisung park <jisng.prk@gmail.com>
2018-12-26 17:19:24 +0900

    dfs

--- DataStructure/dfcs.cpp
+++ DataStructure/dfcs.cpp
@@ -1,24 +1,98 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <string.h>
+#include <queue>
 
 using namespace std;
 
-vector<vector<int>> adjlist;
+vector<int> a[1000];
+queue<int> bfsqueue;
+
+bool visit[1000];
+
+void dfs(int node){
+
+  visit[node] = true;
+  cout<<node+1<<" ";
+  for(int i = 0;i<a[node].size();i++){
+    if(visit[a[node][i]]==false)
+      dfs(a[node][i]);
+  }
+}
+
+
+void bfs(int node){
+
+  visit[node] = true;
+  cout<<node+1<<" ";
+
+  for(int i = 0;i<a[node].size();i++){
+    if(!visit[a[node][i]]){
+      visit[a[node][i]] = true;
+      bfsqueue.push(a[node][i]);
+    }
+  }
+
+  if(!bfsqueue.empty()){
+    int next = bfsqueue.front();
+    bfsqueue.pop();
+    bfs(next);
+  }
+}
+
+void bfs_nonrec(int node){
+
+
+  visit[node] = true;
+  cout<<node+1<<" ";
+
+
+  do{
+
+    for(int i = 0;i<a[node].size();i++){
+      if(!visit[a[node][i]]){
+        visit[a[node][i]] = true;
+        bfsqueue.push(a[node][i]);
+      }
+    }
+
+    node = bfsqueue.front();
+    bfsqueue.pop();
+
+  }while(!bfsqueue.empty());
+
+}
+
 
 
 int main(){
-
-  int nitem = 4;
-  vector<int> element(3,-1);
-  adjlist.push_back(element);
-  vector<int> element2(4,-2);
-  adjlist.push_back(element2);
-  for(int j = 0;j<adjlist.size();j++){
-    for(int i = 0;i<adjlist[j].size();i++){
-      cout<<adjlist[j][i]<<" ";
-    }
-    cout<<endl;
+  int n, m, start;
+  cin>>n>>m>>start;
+  for(int i = 0;i<m;i++){
+    int u,v;
+    cin>>u>>v;
+    a[u-1].push_back(v-1);
+    a[v-1].push_back(u-1);
   }
+
+  for(int i = 0;i<n;i++){
+    sort(a[i].begin(),a[i].end());
+  }
+
+  memset(visit,false,sizeof(visit));
+
+  //dfs(0);
+  //bfs(0);
+  bfs_nonrec(0);
+    // for(int i = 0;i<n;i++){
+  //   cout<<"node: "<<i+1<<endl;
+  //   for(int j = 0;j<a[i].size();j++){
+  //     cout<<a[i][j]+1<<" ";
+  //   }
+  //   cout<<endl;
+  // }
+  // cout<<"done: "<<endl;
 
 
 }
